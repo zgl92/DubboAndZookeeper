14ab1a705823bd7f2fe162bdf6e7177a9cdc77c7
pt310133 <237156698@qq.com>
2018-11-12 14:11:51 +0800

    pt

--- easybuy-parent/easybuy-manager-web/src/main/java/org/java/contorller/ProductController.java
+++ easybuy-parent/easybuy-manager-web/src/main/java/org/java/contorller/ProductController.java
@@ -19,6 +19,7 @@ public class ProductController {
     @RequestMapping("/show/{id}")
     @ResponseBody
     public EasybuyProduct show(@PathVariable("id") Integer id){
+
         return productService.getById(id);
     }
 }
