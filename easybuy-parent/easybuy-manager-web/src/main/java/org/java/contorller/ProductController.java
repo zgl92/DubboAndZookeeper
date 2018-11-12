5e69a065785153a6cd9b92808d66b9b27e9bf357
zgl92 <44958768+zgl92@users.noreply.github.com>
2018-11-12 15:26:17 +0800

    zgl

--- easybuy-parent/easybuy-manager-web/src/main/java/org/java/contorller/ProductController.java
+++ easybuy-parent/easybuy-manager-web/src/main/java/org/java/contorller/ProductController.java
@@ -17,7 +17,7 @@ public class ProductController {
     @RequestMapping("/show/{id}")
     @ResponseBody
     public EasybuyProduct show(@PathVariable("id") Integer id){
-        String bbq="azgl";
+        String bbq="azglss";
         return productService.getById(id);
     }
 }
